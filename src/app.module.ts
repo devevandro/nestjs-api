9a78f55729dcf75d6f5d1c9921ab34fdfca7e175
devevandro <dev.evandro@gmail.com>
2022-01-08 15:50:49 -0300

    Update: remove db url

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/backendtotalzero'),
+    MongooseModule.forRoot(''),
     UsersModule
   ],
   controllers: [AppController],
